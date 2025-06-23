d7d5e9ac874c6c841e033d4fc5e3f711cc7fa895
pranav-sharma-official <159471319+Pranav-Sharma-Official@users.noreply.github.com>
2025-06-23 16:11:13 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Secure Password Generator',
+  description: 'Create a secure strong password.',
+  generator: 'Pranav Sharma',
+  icons: '/favicon.ico',
 }
 
 export default function RootLayout({
